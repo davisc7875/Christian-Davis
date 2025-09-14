daa3a6534c0f87b58ff702fa6b77a38c7353daca
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-09-14 12:52:21 +0000

    finished applelab

---
+++ M1LAB1.cpp
@@ -0,0 +1,40 @@
+/*
+CSC 134
+M1LAB1 *Apple Sales*
+CSC 134
+davisc
+9/14/25
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+// Program acts like a storefront
+
+string item_name "apples";
+int number_of_items 100;
+double cost_each "0.25"
+double total_cost;
+
+//greeting
+cout << "Welcome to the " << item_name << "store!" << endl;
+
+//ask for order
+cout << "we have" << number_of_items << " " << item_name << endl;
+cout << "Each costs $" << cost_each << endl;
+
+//Calculate total price
+total_cost = number_of_items = cost_each;
+
+//output total results
+cout<<"To purchase all" << number_of_items << "will cost $" << total_cost << "total." << endl;
+cout<< "Thank you for shopping with us!" << endl;
+return 0;
+
+
+
+
+
+}
