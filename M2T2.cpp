c3cfb327b87d2927c877863ec5fd4b1ff7ec3003
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-09-27 15:43:07 +0000

    M2t2 update 1

---
+++ M2T2.cpp
@@ -0,0 +1,42 @@
+/*
+CSC 134
+M2T2 Lab
+CSC 134
+davisc
+9/27/25
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//create simple reciept that also handles sales tax
+//declare variables
+string item = "🍕 Pizza";
+double item_price = 5.00;
+double tax_percent = 0.08;
+double tax_amount;
+double total;
+
+//greet user and take order
+cout << "Welcome to our CSC 134 Resturant!!" << endl;
+cout << "You ordered one " << item << ". " << endl;
+//calculate meal price
+//calculate sales tax and total
+tax_amount = item_price * tax_percent;
+total = item_price * tax_amount;
+
+
+
+//print reciept
+cout << setprecision(2) << fixed;
+cout << "Thank you for shopping with us" << endl;
+cout << "______________________________" << endl;
+cout << item << "\t$" << item_price;     << endl;
+cout << "Tax" << "\t\t$" << tax_amount;    << endl;
+cout << "______________________________" << endl;
+cout << "Total" << "\t\t$" << total;       << endl;
+cout << endl;
+
+
+return 0;
