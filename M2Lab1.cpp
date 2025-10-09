8b53be821d8435f9845d6d6423ec9138481729d1
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-10-09 17:39:33 +0000

    Second, potential final

--- M2Lab1.cpp
+++ M2Lab1.cpp
@@ -12,8 +12,13 @@ using namespace std;
 
 int main() {
     
+    /* 🤖 Hello, what tools, and rules will i use for this job?
+    */
+
     const double COST_PER_CUBIC_FOOT = 0.23;
     const double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+    
 
     double length,
         width,
@@ -23,7 +28,10 @@ int main() {
         charge,
         profit;
 
-
+        cout << setprecision(2) << fixed << showpoint;
+    /* 🤖 okay thanks, how do i use these?
+    */
+   // Prompt
     cout << "Please enter the dimensions of the crate in feet" endl;
     cout << "Length?" ;
     cin >> length;
@@ -33,8 +41,19 @@ int main() {
     
     cout << "Height?" ;
     cin >> height;
-
+    // Process
     volume = length * width
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost;
+
+    // Display
+    cout << "The volume of the crate is ";
+    cout << volume << " cubic feet. \n";
+    cout << "Cost to build: $" << cost << endl;
+    cout << "Charge to customer: $" << charge << endl;
+    cout << "Profit: $" << profit << endl;
+    return 0;
 
 return 0;
 }
