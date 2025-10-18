bad74c5583ae9e09dd0f12a32b99bcbfbde6b6b7
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-10-18 16:10:59 +0000

    2nd change

--- M3LAB2 .cpp
+++ M3LAB2 .cpp
@@ -11,10 +11,11 @@ davisc
 using namespace std;
 
 //Set up grade variable
-double grade
+double grade;
 
 //Ask user to input grade
 cout << "Enter your numerical grade:" << endl;
+
 cin >> grade;
 
 //Process the entered grade and output the letter grade
@@ -24,18 +25,20 @@ int main() {
     if (grade <= 100 && grade >= 90) {
     cout << "Your letter grade is: A" << endl;
 }
-    elseif (grade <= 89 && grade >= 80) {
+    else if (grade <= 89 && grade >= 80) {
     cout << "Your letter grade is: B" << endl;
 }
-    elseif (grade <= 79 && grade >= 70) {
+    else if (grade <= 79 && grade >= 70) {
     cout << "Your letter grade is: B" << endl;
 } 
-    elseif (grade <= 69 && grade >= 60) {
+    else if (grade <= 69 && grade >= 60) {
     cout << "Your letter grade is: D" << endl;
 }
-    elseif (grade <= 59 && grade >= 0) {
+    else if (grade <= 59 && grade >= 0) {
     cout << "Your letter grade is: D" << endl;
 }
-
+    else {
+    cout << "Your number grade is outside the valid range" << endl;
+}
  return 0;
 }
