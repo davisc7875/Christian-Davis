d6ffba2920510812e3939cf922798c6a4e2498d2
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-10-18 01:25:11 +0000

    1st change

---
+++ M3LAB1.cpp
@@ -0,0 +1,39 @@
+/*
+M3LAB1
+CSC 134
+davisc
+10/17/25
+
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+string red, green, blue;
+int choice
+
+int main() 
+{
+cout << "Before you are a red, green, and blue barrel," << endl;
+cout << "Choose one of the barrels to shoot by typing their color." << endl;
+
+cin >> choice;
+cout << endl;
+
+  if (red == choice) {
+    cout << "Shooting the red barrel causes it to explode" << endl;
+  }
+  else if (green == choice) {
+    cout << "Shooting the green barrel causes it to leak acid melting its surrounding area"
+  }
+  else if (blue == choice) {
+    cout << "Shooting the blue barrel causes it to leak Liquid Nitrogen freezing its surrounding area"
+  }
+  else {
+    cout << "That's not one of the choices provided" << endl;
+  }
+
+  cout << "Thank you for playing!" << endl;
+  return 0;
+}
