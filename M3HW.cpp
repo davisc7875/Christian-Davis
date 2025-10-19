8bbcc1fe67450f320a7be9fd6d4dfac06cc7da41
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-10-19 13:35:53 +0000

    1st Change

---
+++ M3HW.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M3HW - Silver
+// Christian Davis
+// 10/19/25
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main() {
+
+    string choice
+    cout << "Hi! I'm a C++ program, are you having a good day or a bad day?" << endl;    
+
+    cin >> choice;
+    cout << endl;
+
+  if (choice == "good") {
+    cout << "Thats good to hear! I hope the rest of yur day goes well." << endl;
+  }
+  else if (choice == "bad") {
+    cout << "Im sorry to hear that, hope it gets better soon." << endl;
+  }
+  else {
+    cout << "No worries if you're not sure." << endl;
+  }
+
+  cout << "Thank you for playing!" << endl;
+  return 0;
+}
+
+return 0;
+}
