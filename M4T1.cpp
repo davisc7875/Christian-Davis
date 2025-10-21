3363c78886796ece389e884b90382efe2f82093a
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-10-21 02:23:10 +0000

    Change 1

---
+++ M4T1.cpp
@@ -0,0 +1,21 @@
+//Hello squared
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int MIN_NUMBER = 1,
+              MAX_NUMBER = 5;
+
+    int num = MIN_NUMBER;
+
+    cout << "Hello Number  Number squared\n";
+    cout << "-----------------------------\n";
+    while (num <= MAX_NUMBER)
+    {
+        cout << num << "Hello \t\t" <<  (num * num) << endl;
+        num++;
+    }
+        cout << "That's all!\n";
+        return 0;
+}
