40530c29d81a9eb911be3842ee5e60d908476cdf
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-11-02 18:16:06 +0000

    1st change

---
+++ M4Lab1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "M4LAB, Boxes\n";
+
+    int height, width;
+
+    cout << "How tall is the box?: " << endl;
+    cin >> height;
+    
+     cout << "How wide is the box?: " << endl;
+    cin >> width;
+
+    cout << "one row" << endl;
+    for (int i = 0; i < width; i++) {
+        cout << "*" << " ";
+    }
+    cout << endl;
+
+    cout <<"One column" << endl;
+    for (int j=0; j < height; j++) {
+    
+        cout << "*" << endl;
+    }
+
+    cout << "Box" << endl;
+    for (int j=0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            cout << "*" << " ";
+        }
+        
+    }
+    }
