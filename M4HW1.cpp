67b94b33306660bbe3588a9d088e2144a10d7365
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-11-02 19:26:58 +0000

    1st change

---
+++ M4HW1.cpp
@@ -0,0 +1,23 @@
+/*
+M4HW
+CSC 134
+davisc
+11/2/25
+Multiplication Table
+Silver rank
+*/
+
+// This program asks the user for a number between 1 and 12, afterwards multiplying the number in order from 1 to 12
+// This program will use loops that stop after 12 iterations
+#include <iostream>
+using namespace std;
+
+int num1, num2, answer;
+cout << "Enter your number: " << endl;
+cin >> num1;
+
+for (int i = 1; i <= 12; i++) {
+    answer = num1 * i;
+    cout << num1 << " x " << i << " is " << answer << endl;
+    
+}
