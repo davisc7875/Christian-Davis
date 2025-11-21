d03ec1d1ee055dd5da1c4c93612dee64f2fc02ff
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-11-21 15:54:50 +0000

    Change 2

---
+++ M5Lab2.cpp
@@ -0,0 +1,52 @@
+// M5LAB2
+// Header goes here
+
+#include <iostream>
+#include <limits>
+#include <iomanip>
+using namespace std;
+
+double getLength();
+double getWidth();
+double getArea(double length, double width);
+void displayData(double length, double width, double area);
+
+int main(){
+	
+   double length,    // The rectangle's length
+          width,    // The rectangle's width
+          area;      // The rectangle's area
+          
+   length = getLength();
+   width = getWidth();
+
+   area = getArea(length, width);
+
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+double getLength() {
+    double len;
+    cout << "Please enter your length: ";
+}
+
+double getWidth() {
+    double wid;
+    cout << "Please enter your width: ";
+}
+
+double getArea(double length, double width) {
+    return length * width;
+}
+
+void displayData(double length, double width, double area) {
+    cout << "Length: " << length << '\n';
+    cout << "Width : " << width  << '\n';
+    cout << "Area  : " << area   << '\n';
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
