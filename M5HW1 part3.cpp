a1a48efcc38616c77e4209e947927633d8707d33
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-11-24 21:57:46 +0000

    Change 2

--- M5HW1 part3.cpp
+++ M5HW1 part3.cpp
@@ -4,23 +4,51 @@ int main() {
     int number;
     string rnumeral;
 
-    I
-    II
-    III
-    IV
-    V
-    VI
-    VII
-    VIII
-    IX
-    X
-
 cout << "Enter a number between 1 and 10" << endl;
 cin >> number;
 
 if (number == 1)
 {
-    rnumeral == I
+    rnumeral == "I";
+}
+else if (number == 2)
+{
+    rnumeral == "II";
+}
+else if (number == 3)
+{
+    rnumeral == "III";
+}
+else if (number == 4)
+{
+    rnumeral == "IV";
+}
+else if (number == 5)
+{
+    rnumeral == "V";
+}
+else if (number == 6)
+{
+    rnumeral == "VI";
+}
+else if (number == 7)
+{
+    rnumeral == "VII";
+}
+else if (number == 8)
+{
+    rnumeral == "VIII";
+}
+else if (number == 9)
+{
+    rnumeral == "IX";
+}
+else if (number == 10)
+{
+    rnumeral == "X";
+}
+else {
+    cout << "Your number is over or under the numbers 1 - 10" << endl;
 }
 cout << "The Roman Numeral version of " << number <<  "is" << rnumeral << endl;
 
