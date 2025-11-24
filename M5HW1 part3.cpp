95bc7ec9599d3f2b746108d5abc2167f94809a28
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-11-24 21:52:42 +0000

    Change 1

---
+++ M5HW1 part3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    string rnumeral;
+
+    I
+    II
+    III
+    IV
+    V
+    VI
+    VII
+    VIII
+    IX
+    X
+
+cout << "Enter a number between 1 and 10" << endl;
+cin >> number;
+
+if (number == 1)
+{
+    rnumeral == I
+}
+cout << "The Roman Numeral version of " << number <<  "is" << rnumeral << endl;
+
+return 0;
+}
