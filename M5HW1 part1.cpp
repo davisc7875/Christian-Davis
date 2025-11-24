3fc750794f9ea47570e8a925681d7c98208d7922
Christian Davis <43657088+davisc7875@users.noreply.github.com>
2025-11-24 21:05:56 +0000

    first change

---
+++ M5HW1 part1.cpp
@@ -0,0 +1,37 @@
+/*
+This program uses a loop to prompt the user multiple times and then uses those inputs to calculate the average rainfall for 3 months
+
+every time a month is added by an if statement, counter goes up, when the counter goes to 3, the average calculation is triggered and the program ends
+If you enter a month twice */
+#include <iostream>
+using namespace std;
+int main() {
+int counter = 0;
+
+string month, january , february, march;
+double jan_rf, feb_rf, mar_rf, avg_rainfall;
+
+while (counter < 3) {
+cout << "Enter a month from January to March: ";
+cin >> month;
+
+
+if (month == "january") {
+    cout << "Enter rainfall for January: ";
+    cin >> jan_rf;
+  } else if (month == "february") {
+    cout << "Enter rainfall for February: ";
+    cin >> feb_rf;
+  } else if (month == "march") {
+    cout << "Enter rainfall for March: ";
+    cin >> mar_rf;
+  } else {
+    cout << "That's not a valid choice, please try again." << endl;
+  }
+}
+
+cout << "the average rainfall for January, February, and March is " << avg_rainfall << "inches." << endl;
+counter++;
+
+return 0;
+};
